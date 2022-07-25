9622dde33e4b2c2b1997256bee28fb5010621f96
MaxPower <andrewrubanov@gmail.com>
2022-07-25 09:55:48 +0600

    выполненные задания с 1 по 4

---
+++ task4/js/script.js
@@ -0,0 +1,7 @@
+var inputtel = document.getElementById("telphone");
+var maskTel = {
+    mask: '+7(000)000-00-00',
+    lazy: false,
+};
+var phone = new IMask(inputtel, maskTel);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHQuanMiXSwic291cmNlc0NvbnRlbnQiOlsidmFyIGlucHV0dGVsID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJ0ZWxwaG9uZVwiKTtcclxudmFyIG1hc2tUZWwgPSB7XHJcbiAgICBtYXNrOiAnKzcoMDAwKTAwMC0wMC0wMCcsXHJcbiAgICBsYXp5OiBmYWxzZSxcclxufTtcclxudmFyIHBob25lID0gbmV3IElNYXNrKGlucHV0dGVsLCBtYXNrVGVsKTtcclxuIl0sImZpbGUiOiJzY3JpcHQuanMifQ==
